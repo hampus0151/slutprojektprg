b950d253c7ca3b918a49640f7c88ad860a9e87ba
hampus0151 <hampus.nasvall@gmail.com>
2020-05-12 10:59:39 +0200

    gui

---
+++ game/src/slutpojekt.java
@@ -0,0 +1,82 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class slutpojekt extends javax.swing.JFrame {
+
+    private JLabel treirad;
+    private JButton a9Button;
+    private JButton a7Button;
+    private JButton a6Button;
+    private JButton a4Button;
+    private JButton a8Button;
+    private JButton a5Button;
+    private JButton highscoresButton;
+    private JButton restartButton;
+    private JButton a1Button;
+    private JButton a2Button1;
+    private JButton a3Button1;
+
+
+    public slutpojekt() {
+        a1Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        a2Button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        a3Button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        a4Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        a5Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        a6Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        a7Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        a8Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        a9Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+    private void createUIComponents() {
+    setSize(1200,600);
+    setLocationRelativeTo(null);
+    }
+}
